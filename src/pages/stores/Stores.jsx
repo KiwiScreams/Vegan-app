b7c1619c6063dec535abe5ad823bd4a9945f4c52
KiwiScreams <screamskiwi3@gmail.com>
2024-08-18 21:51:20 +0400

    map

--- src/pages/stores/Stores.jsx
+++ src/pages/stores/Stores.jsx
@@ -2,48 +2,45 @@ import "./Stores.css";
 import storeImage from "../../assets/img/shop.svg";
 import locationImage from "../../assets/img/placeholder 1.svg";
 const Stores = () => {
+  const storesData = [
+    {
+      city: "თბილისი",
+      address: "ი. აბაშიძის 56",
+    },
+    {
+      city: "თბილისი",
+      address: "ი. აბაშიძის 56",
+    },
+    {
+      city: "თბილისი",
+      address: "ი. აბაშიძის 56",
+    },
+    {
+      city: "თბილისი",
+      address: "ი. აბაშიძის 56",
+    },
+  ];
   return (
     <>
       <section className="stores-section">
         <div className="store-content">
           <h2>გვესტუმრეთ ფილიალში</h2>
           <div class="store-container flex">
-            <div class="store-box">
-              <img src={storeImage} alt="shop" class="shop white-on-hover" />
-              <p>
-                თბილისი
-                <br />
-                ი. აბაშიძის 56
-              </p>
-              <img src={locationImage} alt="location" class="location" />
-            </div>
-            <div class="store-box">
-              <img src={storeImage} alt="shop" class="shop white-on-hover" />
-              <p>
-                თბილისი
-                <br />
-                ი. აბაშიძის 56
-              </p>
-              <img src={locationImage} alt="location" class="location" />
-            </div>
-            <div class="store-box">
-              <img src={storeImage} alt="shop" class="shop white-on-hover" />
-              <p>
-                თბილისი
-                <br />
-                ი. აბაშიძის 56
-              </p>
-              <img src={locationImage} alt="location" class="location" />
-            </div>
-            <div class="store-box">
-              <img src={storeImage} alt="shop" class="shop white-on-hover" />
-              <p>
-                თბილისი
-                <br />
-                ი. აბაშიძის 56
-              </p>
-              <img src={locationImage} alt="location" class="location" />
-            </div>
+            {storesData.map((store, index) => (
+              <div key={index} className="store-box">
+                <img
+                  src={storeImage}
+                  alt="shop"
+                  className="shop white-on-hover"
+                />
+                <p>
+                  {store.city}
+                  <br />
+                  {store.address}
+                </p>
+                <img src={locationImage} alt="location" className="location" />
+              </div>
+            ))}
           </div>
         </div>
       </section>
