1e6a26c935b2dde89b9870818b913d3dade4d4c3
KiwiScreams <screamskiwi3@gmail.com>
2024-08-18 20:18:07 +0400

    header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,10 +1,40 @@
 import "./Header.css";
+import { NavLink } from "react-router-dom";
 const Header = () => {
-    return ( 
-        <>
-        
-        </>
-     );
-}
- 
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className="flex">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                ვეგანური პროდუქცია
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/stores"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                მაღაზიები
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/shop"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                shop
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
 export default Header;
