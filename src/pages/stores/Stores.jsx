5480c3d321c7e581695c99c6c764b505f2db7c39
KiwiScreams <screamskiwi3@gmail.com>
2024-08-18 21:40:23 +0400

    stores

--- src/pages/stores/Stores.jsx
+++ src/pages/stores/Stores.jsx
@@ -1,9 +1,32 @@
 import "./Stores.css";
+import storeImage from "../../assets/img/shop.svg"
 const Stores = () => {
     return ( 
         <>
         <section className="stores-section">
-            
+            <p>გვესტუმრეთ ფილიალში</p>
+            <div class="box-container flex">
+                <div class="box">
+                    <img src="./img/shop (1) 1.svg" alt="shop" class="shop white-on-hover"/>
+                    <p>თბილისი<br/>ი. აბაშიძის 56</p>
+                    <img src="./img/placeholder 1.svg" alt="location" class="location"/>
+                </div>
+                <div class="box">
+                    <img src="./img/shop (1) 1.svg" alt="shop" class="shop white-on-hover"/>
+                    <p>თბილისი<br/>ი. აბაშიძის 56</p>
+                    <img src="./img/placeholder 1.svg" alt="location" class="location"/>
+                </div>
+                <div class="box">
+                    <img src="./img/shop (1) 1.svg" alt="shop" class="shop white-on-hover"/>
+                    <p>თბილისი<br/>ი. აბაშიძის 56</p>
+                    <img src="./img/placeholder 1.svg" alt="location" class="location"/>
+                </div>
+                <div class="box">
+                    <img src="./img/shop (1) 1.svg" alt="shop" class="shop white-on-hover"/>
+                    <p>თბილისი<br/>ი. აბაშიძის 56</p>
+                    <img src="./img/placeholder 1.svg" alt="location" class="location"/>
+                </div>
+            </div>
         </section>
         </>
      );
