41f7d5b391294c46162e425bd324a5406a11e1dc
KiwiScreams <screamskiwi3@gmail.com>
2024-08-20 13:48:20 +0400

    multifilter works!

--- src/components/product list/ProductList.jsx
+++ src/components/product list/ProductList.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import { products } from "../../data/data";
+import { Link, NavLink } from "react-router-dom";
 const ProductList = () => {
   const updatedProducts = products.map((product) => ({
     ...product,
-    categories: Array.isArray(product.category) ? product.category : [product.category],
+    categories: Array.isArray(product.category)
+      ? product.category
+      : [product.category],
   }));
   const [selectedFilters, setSelectedFilters] = useState([]);
   const [filteredItems, setFilteredItems] = useState(updatedProducts);
@@ -14,7 +17,7 @@ const ProductList = () => {
     { category: "Superfood", label: "სუპერფუდი" },
     { category: "Vitamin", label: "ვიტამინები" },
     { category: "Diabet", label: "დიაბეტური კეტო დიეტა" },
-    { category: "Hygiene", label: "სახლის ჰიგიენა სარეცხი სითხეები" }
+    { category: "Hygiene", label: "სახლის ჰიგიენა სარეცხი სითხეები" },
   ];
   const categoryMap = filters.reduce((acc, curr) => {
     acc[curr.category] = curr.label;
@@ -35,21 +38,59 @@ const ProductList = () => {
 
   const filterItems = () => {
     if (selectedFilters.length > 0) {
-      const filteredItems = updatedBlogs.filter((blog) => {
+      const filteredItems = updatedProducts.filter((blog) => {
         return selectedFilters.every((filter) =>
           blog.categories.includes(filter)
         );
       });
       setFilteredItems(filteredItems);
     } else {
-      setFilteredItems(updatedBlogs);
+      setFilteredItems(updatedProducts);
     }
   };
   const getCategoryClassName = (category) => {
     const filter = filters.find((filter) => filter.category === category);
     return `category-${filters.indexOf(filter) + 1}`;
   };
-  return <></>;
+  return (
+    <>
+      <section>
+        <section>
+          {filters.map((filter, idx) => (
+            <button
+              onClick={() => handleFilterButtonClick(filter.category)}
+              className={`button ${
+                selectedFilters?.includes(filter.category)
+                  ? "active-filter"
+                  : ""
+              }`}
+              key={`filters-${idx}`}
+            >
+              {filter.label}
+            </button>
+          ))}
+        </section>
+
+        <section>
+          {filteredItems.length > 0 ? (
+            filteredItems.map((item, idx) => (
+              <div key={`blogs-${idx}`}>
+                <div>
+                  <img src={item.image} alt="" />
+                </div>
+                <div>
+                  <p>{item.title}</p>
+                  <Link to={`/blog/${item.id}`}>სრულად ნახვა</Link>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="no-category">ასეთი პროდუქტი არ არსებობს</p>
+          )}
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default ProductList;
