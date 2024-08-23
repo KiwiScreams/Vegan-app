7b405123aa29afe7f0a38881c12d32a4b907c909
KiwiScreams <screamskiwi3@gmail.com>
2024-08-23 18:13:50 +0400

    loader no-scroll

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,22 +2,30 @@ import { useEffect, useState } from "react";
 import "./Loader.css";
 const Loader = () => {
   const [show, setShow] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
       setShow(false);
-      setText("lol");
     }, 3000);
   }, []);
+
+  useEffect(() => {
+    if (show) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [show]);
+
   return (
     <>
       <div>
-        {
-        show ? 
-        <section className="loader">
+        {show ? (
+          <section className="loader">
             <h1>Vegan World</h1>
-        </section> : null
-        }
-        </div>
+          </section>
+        ) : null}
+      </div>
     </>
   );
 };
