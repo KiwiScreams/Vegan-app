63128596bb3e53cb04f4f2dcd3d11bb9f507fbd8
KiwiScreams <screamskiwi3@gmail.com>
2024-09-01 20:50:27 +0400

    loader in product detail page

--- src/components/product detail/ProductDetail.jsx
+++ src/components/product detail/ProductDetail.jsx
@@ -6,12 +6,15 @@ import RelatedList from "../related list/RelatedList";
 import Cart from "../cart/Cart";
 import Panel from "../panel/Panel";
 import WishList from "../wishlist/WishList";
+import Loader from "../loader/Loader";
 const ProductDetail = ({ onAddToCart, onAddToWishList }) => {
   const { id } = useParams();
   const productId = parseInt(id, 10);
   const product = products.find((product) => product.id === productId);
   const [showPanel, setShowPanel] = useState(false);
   const [cardMessage, setCardMessage] = useState("");
+  const [loading, setLoading] = useState(true);
+
   const relatedProducts = products
     .filter((relatedProduct) => {
       if (relatedProduct.id === productId) return false;
@@ -44,73 +47,84 @@ const ProductDetail = ({ onAddToCart, onAddToWishList }) => {
   const handleMouseOut = () => {
     setIsHovered(false);
   };
-
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <section className="product-detail-section">
-        <div className="detail-container">
-          <div className="image-container">
-            <img src={product.image} alt="" />
-            {product.discount > 0 && (
-              <div className="discount-text">
-                <p>-{product.discount}%</p>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="product-detail-section">
+          <div className="detail-container">
+            <div className="image-container">
+              <img src={product.image} alt="" />
+              {product.discount > 0 && (
+                <div className="discount-text">
+                  <p>-{product.discount}%</p>
+                </div>
+              )}
+            </div>
+            <div className="detail-info-container">
+              <h1>
+                <span className="first-letter">{product.title.charAt(0)}</span>
+                {product.title.slice(1)}
+              </h1>
+              <div className="flex">
+                <p className="price">
+                  ფასი:
+                  <span>
+                    {product.discount > 0
+                      ? (
+                          product.price -
+                          (product.price * product.discount) / 100
+                        ).toFixed(2)
+                      : product.price.toFixed(2)}
+                    <i className="fa-solid fa-lari-sign"></i>
+                  </span>
+                </p>
+                <div className="grade flex">
+                  <i
+                    className={`fa-star ${
+                      isHovered ? "fa-solid" : "fa-regular"
+                    }`}
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                  ></i>
+                  <p>{product.grade}</p>
+                </div>
               </div>
-            )}
-          </div>
-          <div className="detail-info-container">
-            <h1>
-              <span className="first-letter">{product.title.charAt(0)}</span>
-              {product.title.slice(1)}
-            </h1>
-            <div className="flex">
-              <p className="price">
-                ფასი:
-                <span>
-                  {product.discount > 0
-                    ? (
-                        product.price -
-                        (product.price * product.discount) / 100
-                      ).toFixed(2)
-                    : product.price.toFixed(2)}
+              {product.discount > 0 ? (
+                <p className="old-price">
+                  ძველი ფასი: {product.price.toFixed(2)}
                   <i className="fa-solid fa-lari-sign"></i>
-                </span>
-              </p>
-              <div className="grade flex">
-                <i
-                  className={`fa-star ${isHovered ? "fa-solid" : "fa-regular"}`}
-                  onMouseOver={handleMouseOver}
-                  onMouseOut={handleMouseOut}
-                ></i>
-                <p>{product.grade}</p>
+                </p>
+              ) : (
+                <></>
+              )}
+              <p className="description">{product.description}</p>
+              <div className="flex add-list-container">
+                <button
+                  onClick={() => handleAddToCart(product)}
+                  className="add-to-cart"
+                >
+                  კალათაში დამატება
+                </button>
+
+                <button
+                  onClick={() => handleAddToWishList(product)}
+                  className="wishlist-btn"
+                >
+                  <i className="fa-regular fa-heart"></i>
+                </button>
               </div>
             </div>
-            {product.discount > 0 ? (
-              <p className="old-price">
-                ძველი ფასი: {product.price.toFixed(2)}
-                <i className="fa-solid fa-lari-sign"></i>
-              </p>
-            ) : (
-              <></>
-            )}
-            <p className="description">{product.description}</p>
-            <div className="flex add-list-container">
-              <button
-                onClick={() => handleAddToCart(product)}
-                className="add-to-cart"
-              >
-                კალათაში დამატება
-              </button>
-
-              <button
-                onClick={() => handleAddToWishList(product)}
-                className="wishlist-btn"
-              >
-                <i className="fa-regular fa-heart"></i>
-              </button>
-            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
       <RelatedList relatedProducts={relatedProducts} />
       <Panel show={showPanel} message={cardMessage} />
     </>
